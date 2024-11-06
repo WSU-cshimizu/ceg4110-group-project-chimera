550796c0a8fe8aa4fca6c74849b617edc5f71a96
Irlivingstonn <irlivingstonn@gmail.com>
2024-11-06 17:51:04 -0500

    Add locations and kwn_etity sql files

---
+++ code/chimera-backend-expressjs/Database/locations.sql
@@ -0,0 +1,97 @@
+INSERT INTO location
+Values (01,
+  'Library',
+  001,
+  'Library Basement Storage Room and Breaker');
+  
+INSERT INTO location
+Values (02,
+  'Library',
+  002,
+  'Library Basement Lab');
+ 
+INSERT INTO location
+Values (03,
+  'Library',
+  101,
+  'Librarians Office');
+  
+INSERT INTO location
+Values (04,
+  'Library',
+  102,
+  'Library Front Desk');
+  
+INSERT INTO location
+Values (05,
+  'Library',
+  103,
+  'Library Main Lobby');
+  
+INSERT INTO location
+Values (06,
+  'Library',
+  201,
+  'Library Archives');
+  
+INSERT INTO location
+Values (07,
+  'Library',
+  202,
+  'Library Fiction Section');
+  
+ 
+INSERT INTO location
+Values (08,
+  'Library',
+  203,
+  'Library Nonfiction Section');
+  
+ 
+INSERT INTO location
+Values (09,
+  'Oelman Hall',
+  001,
+  'Oelman Hall Basement Storage Room and Breaker');
+  
+INSERT INTO location
+Values (10,
+  'Oelman Hall',
+  002,
+  'Oelman Hall Basement Boiler Room');
+  
+INSERT INTO location
+Values (11,
+  'Oelman Hall',
+  101,
+  'Oelman Hall 101 Lecture Hall');
+  
+INSERT INTO location
+Values (12,
+  'Oelman Hall',
+  102,
+  'Oelman Hall 102 Lab');
+  
+INSERT INTO location
+Values (13,
+  'Oelman Hall',
+  103,
+  'Oelman Hall 103 Lecture Hall');
+  
+INSERT INTO location
+Values (14,
+  'Oelman Hall',
+  201,
+  'Oelman Hall 201 Chem Lab');
+  
+INSERT INTO location
+Values (15,
+  'Oelman Hall',
+  202,
+  'Oelman Hall 202 Male Restrooms');
+  
+INSERT INTO location
+Values (16,
+  'Oelman Hall',
+  203,
+  'Oelman Hall 203 Female Restrooms');
