7df47b7ae5c26ace29ff2f7afd8800d258b5b823
Irlivingstonn <irlivingstonn@gmail.com>
2024-11-06 15:17:50 -0500

    Updated server set up to avoid auth issues

--- code/chimera-backend-expressjs/config/db.config.js
+++ code/chimera-backend-expressjs/config/db.config.js
@@ -2,5 +2,8 @@ module.exports = {
     host: "localhost",
     user: "chimera_user",
     password: "1234",
-    database: "chimera_db"
+    database: "chimera_db",
+    authPlugins: {
+      mysql_native_password: () => () => Buffer.from('1234')
+    }
   };
