aafa2a5a3ed889c8d8a2b3d3cedb45a2d0d70216
Irlivingstonn <irlivingstonn@gmail.com>
2024-11-06 17:04:16 -0500

    Added match querys for megan

--- code/chimera-backend-expressjs/routes/entities.js
+++ code/chimera-backend-expressjs/routes/entities.js
@@ -77,4 +77,64 @@ router.delete('/entities/:id', (req, res) => {
 });
 });
 
+
+// ------------------ FOR MEGAN ------------------
+// Compare reported entity with known entities to get parameters
+router.get('/entities/compare/:id', (req, res) => {
+  const reportedEntityId = req.params.id;
+  
+  // Query to get reported entity details
+  db.query('SELECT * FROM rpt_entity WHERE rptid = ?', [reportedEntityId], (err, reportedEntity) => {
+      if (err) {
+          res.status(500).send(err);
+          return;
+      }
+      
+      // Query to get all known entities for comparison
+      db.query('SELECT * FROM kwn_entity', (err, knownEntities) => {
+          if (err) {
+              res.status(500).send(err);
+              return;
+          }
+
+          // Data used for matching
+          res.send({
+              reportedEntity: reportedEntity[0],
+              knownEntities: knownEntities
+          });
+      });
+  });
+});
+
+// Store entity match results
+router.post('/entities/matches', (req, res) => {
+  const matchData = {
+      reported_entity_id: req.body.reportedEntityId,
+      known_entity_id: req.body.knownEntityId,
+      match_score: req.body.matchScore,
+      match_criteria: req.body.criteria
+  };
+  
+  db.query('INSERT INTO entity_matches SET ?', matchData, (err, result) => {
+      if (err) {
+          res.status(500).send(err);
+      } else {
+          res.status(201).send({ id: result.insertId, ...matchData });
+      }
+  });
+});
+
+// Get match history for a reported entity
+router.get('/entities/matches/:id', (req, res) => {
+  db.query('SELECT * FROM entity_matches WHERE reported_entity_id = ?', 
+      [req.params.id], 
+      (err, results) => {
+          if (err) {
+              res.status(500).send(err);
+          } else {
+              res.send(results);
+          }
+  });
+});
+
 module.exports = router;
